f0e6d197bd3dfd3387c8884627b2e804b627a99b
Igor Bonfim <83429569+IgormBonfim@users.noreply.github.com>
2022-09-05 16:17:45 -0300

    API

---
+++ Autoplay.API/Program.cs
@@ -0,0 +1,37 @@
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISessionFactory>(factory =>
+{    
+    string connectionString = builder.Configuration.GetConnectionString("MySql");
+    return Fluently.Configure().Database(MySQLConfiguration.Standard
+                                            .ConnectionString(connectionString)
+                                            .ShowSql())
+                   .BuildSessionFactory();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
